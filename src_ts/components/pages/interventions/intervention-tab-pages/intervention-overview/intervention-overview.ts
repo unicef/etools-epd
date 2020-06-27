1c05028d6bfede2b555ecf1b6bd2053df9afa771
Adriana Trif <trif.corina.adriana@gmail.com>
2020-06-27 22:06:55 +0300

    Tabs setup

---
+++ src_ts/components/pages/interventions/intervention-tab-pages/intervention-overview/intervention-overview.ts
@@ -0,0 +1,16 @@
+import {LitElement, customElement, html} from 'lit-element';
+
+/**
+ * @customElement
+ */
+@customElement('intervention-overview')
+export class InterventionOverview extends LitElement {
+  render() {
+    // language=HTML
+    return html`
+      <style></style>
+
+      Overview page
+    `;
+  }
+}
---
+++ src_ts/components/pages/interventions/intervention-tab-pages/intervention-results/intervention-results.ts
@@ -0,0 +1,16 @@
+import {LitElement, customElement, html} from 'lit-element';
+
+/**
+ * @customElement
+ */
+@customElement('intervention-results')
+export class InterventionResults extends LitElement {
+  render() {
+    // language=HTML
+    return html`
+      <style></style>
+
+      Results page
+    `;
+  }
+}
