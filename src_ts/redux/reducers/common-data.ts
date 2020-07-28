01e59c0d085b7d4099164dca9cc1f040d5ced348
Uladzimir Urbanovich <v_urbanovich@mail.ru>
2020-07-28 10:45:13 +0300

    Fix rebase conflicts

--- src_ts/redux/reducers/common-data.ts
+++ src_ts/redux/reducers/common-data.ts
@@ -1,17 +1,5 @@
 import {Reducer} from 'redux';
-import {
-  SET_UNICEF_USERS,
-  SET_PARTNERS,
-  SET_LOCATIONS,
-  SET_LOCATION_TYPES,
-  SET_DOCUMENT_TYPES,
-  SET_GENDER_EQUITY_RATINGS,
-  SET_SECTIONS,
-  SET_DISAGGREGATIONS,
-  SET_OFFICES,
-  SET_ALL_STATIC_DATA,
-  SET_CP_OUTPUTS
-} from '../actions/common-data';
+import {SET_UNICEF_USERS, SET_ALL_STATIC_DATA} from '../actions/common-data';
 import {RootAction} from '../store';
 import {CpOutput, Disaggregation, LocationObject, Section, LabelAndValue} from '../../types/globals';
 
@@ -50,50 +38,9 @@ const commonData: Reducer<CommonDataState, RootAction> = (state = INITIAL_COMMON
         ...state,
         unicefUsers: action.unicefUsers
       };
-    case SET_PARTNERS:
-      return {
-        ...state,
-        partners: action.partners
-      };
-    case SET_LOCATIONS:
-      return {
-        ...state,
-        locations: action.locations
-      };
-    case SET_SECTIONS:
-      return {
-        ...state,
-        sections: action.sections
-      };
-    case SET_DISAGGREGATIONS:
-      return {
-        ...state,
-        disaggregations: action.disaggregations
-      };
-    case SET_LOCATION_TYPES:
-      return {
-        ...state,
-        locationTypes: action.locationTypes
-      };
-    case SET_DOCUMENT_TYPES:
-      return {
-        ...state,
-        documentTypes: action.documentTypes
-      };
-    case SET_GENDER_EQUITY_RATINGS:
-      return {
-        ...state,
-        genderEquityRatings: action.genderEquityRatings
-      };
-    case SET_OFFICES:
-      return {
-        ...state,
-        sections: action.offices
-      };
     case SET_ALL_STATIC_DATA:
       return {
         ...state,
-        unicefUsers: action.staticData.unicefUsers,
         partners: action.staticData.partners,
         locations: action.staticData.locations,
         sections: action.staticData.sections,
@@ -101,24 +48,9 @@ const commonData: Reducer<CommonDataState, RootAction> = (state = INITIAL_COMMON
         locationTypes: action.staticData.locationTypes,
         documentTypes: action.staticData.documentTypes,
         genderEquityRatings: action.staticData.genderEquityRatings,
+        cpOutputs: action.staticData.cpOutputs,
         interventionAmendmentTypes: action.staticData.interventionAmendmentTypes,
         offices: action.staticData.offices
-      };
-    case SET_CP_OUTPUTS:
-      return {
-        ...state,
-        cpOutputs: action.cpOutputs
-      };
-    case SET_ALL_STATIC_DATA:
-      return {
-        ...state,
-        partners: action.staticData.partners,
-        locations: action.staticData.locations,
-        sections: action.staticData.sections,
-        disaggregations: action.staticData.disaggregations,
-        locationTypes: action.staticData.locationTypes,
-        documentTypes: action.staticData.documentTypes,
-        genderEquityRatings: action.staticData.genderEquityRatings
       };
     default:
       return state;
