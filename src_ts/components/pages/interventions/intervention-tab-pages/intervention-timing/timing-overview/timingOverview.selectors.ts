b903dbe4673c6c342ee1f4bc67d494e1275b4790
Adriana Trif <trif.corina.adriana@gmail.com>
2020-08-12 09:41:57 +0300

    Rename folder

--- src_ts/components/pages/interventions/intervention-tab-pages/intervention-timing/timing-overview/timingOverview.selectors.ts
+++
@@ -1,8 +0,0 @@
-import {createSelector} from 'reselect';
-import {Intervention} from '../../common/models/intervention.types';
-import {TimingOverviewData} from './timingOverview.models';
-import {currentIntervention} from '../../common/selectors';
-
-export const selectTimingOverview = createSelector(currentIntervention, (intervention: Intervention) => {
-  return new TimingOverviewData(intervention);
-});
