d27399e04942d624fedab94f2b1a733bdb8b5a19
dcioara <dan.cioara@nordlogic.com>
2020-12-11 12:46:48 +0200

    updated dialog version, css fix

--- src_ts/components/app-shell/app-theme.ts
+++ src_ts/components/app-shell/app-theme.ts
@@ -106,7 +106,7 @@ documentContainer.innerHTML = `
           cursor: pointer;
         };
 
-        *:focus:not(paper-icon-button) {
+        *:focus:not(paper-icon-button):not(paper-radio-button) {
           outline: 0;
           box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.4);
         }
