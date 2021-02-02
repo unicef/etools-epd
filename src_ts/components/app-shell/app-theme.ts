d8bfd9474783389361824b57a85ec093ed1930c1
lajos <kovago.lajos91@gmail.com>
2021-02-02 13:16:29 +0200

    fixed buttons focus styles

--- src_ts/components/app-shell/app-theme.ts
+++ src_ts/components/app-shell/app-theme.ts
@@ -113,12 +113,14 @@ documentContainer.innerHTML = `
 
         --paper-button-flat-keyboard-focus: {
           outline: 0;
-          box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.4);
+          box-shadow:  0 0 10px 10px rgba(170, 165, 165, 0.2) !important;
+          background-color: rgba(170, 165, 165, 0.2);
         }
 
         --paper-button-raised-keyboard-focus: {
           outline: 0;
-          box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.4);
+          box-shadow:  0 0 10px 10px rgba(170, 165, 165, 0.2) !important;
+          background-color: rgba(170, 165, 165, 0.2);
         }
 
       }
