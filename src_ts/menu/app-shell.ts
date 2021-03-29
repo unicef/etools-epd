6f895794ec2c5bae36e67b302035b8dfcbaf5832
Adriana Trif <trif.corina.adriana@gmail.com>
2021-03-29 20:44:39 +0300

    display icons

--- src_ts/menu/app-shell.ts
+++ src_ts/menu/app-shell.ts
@@ -1,7 +1,20 @@
 import {css, customElement, html, LitElement} from 'lit-element';
+import {
+  apdIcon,
+  dashIcon,
+  externalIcon,
+  famIcon,
+  fmIcon,
+  pmpIcon,
+  powerBiIcon,
+  pseaIcon,
+  tpmIcon,
+  tripsIcon,
+  unppIcon
+} from './app-selector-icons.js';
 
-@customElement('app-shell')
-export class AppShell extends LitElement {
+@customElement('app-shell2')
+export class AppShell2 extends LitElement {
   static get styles() {
     return [
       css`
@@ -11,14 +24,32 @@ export class AppShell extends LitElement {
         }
         .logo {
           text-align: center;
+          padding: 40px 0;
         }
         .content-container {
+          width: 100%;
+          max-width: 450px;
         }
         .apps-container {
           display: flex;
           flex-direction: row;
           flex-wrap: wrap;
           justify-content: space-around;
+        }
+
+        .layout-h {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+        }
+        .icon-wrapper {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .category {
+          padding-top: 26px;
+          padding-bottom: 16px;
         }
       `
     ];
@@ -29,14 +60,53 @@ export class AppShell extends LitElement {
     // language=HTML
     return html`
       <div class="header-container">
-        <img src="./images/perm_identity-24px.svg" />
+        <img src="./images/perm_identity-24px.svg" alt="etools Logo" />
       </div>
       <div class="logo">
         <img id="app-logo" class="logo" src="./images/etools-logo-color-white.svg" alt="eTools" />
       </div>
-      <div class="content-container">
-        <div class="apps-container">
-
+      <div class="layout-h">
+        <div class="content-container">
+          <div class="category">Programme Management</div>
+          <div class="apps-container">
+            <div class="icon-wrapper">
+              ${unppIcon}
+              UN Partner Portal
+            </div>
+            <div class="icon-wrapper">
+              ${pmpIcon}
+              Partnership Management
+            </div>
+          </div>
+          <div class="category">Monitoring & Assurance</div>
+          <div class="apps-container">
+            <div class="icon-wrapper">
+              ${tripsIcon}
+              Trip Management
+            </div>
+            <div class="icon-wrapper">
+              ${tpmIcon}
+              Third Party Monitoring
+            </div>
+            <div class="icon-wrapper">
+              ${famIcon}
+              Financial Assurance
+            </div>
+            <div class="icon-wrapper">
+              ${pseaIcon}
+              PSEA Assurance
+            </div>
+            <div class="icon-wrapper">
+              ${fmIcon}
+              Field Monitoring
+            </div>
+          </div>
+          <div class="category">Dashborads & Analytics</div>
+          <div class="apps-container">
+          <div class="icon-wrapper">${apdIcon} Action Points</div>
+          <div class="icon-wrapper">${dashIcon} Dashboards</div>
+          <div class="icon-wrapper">${powerBiIcon} <div>Implementation Intelligence (I<sup>2</sup>)</div></div>
+          <div class="icon-wrapper">${externalIcon} Datamart</div>
         </div>
       </div>
     `;
