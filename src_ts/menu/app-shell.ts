ed2a7e2b3cb7abaa2df6c482dfe1833f9df6fa9f
Trif Adriana <trif.corina.adriana@gmail.com>
2021-03-31 17:00:46 +0300

    layout

--- src_ts/menu/app-shell.ts
+++ src_ts/menu/app-shell.ts
@@ -12,6 +12,7 @@ import {
   tripsIcon,
   unppIcon
 } from './app-selector-icons.js';
+import appTheme from './app-theme.js';
 
 @customElement('app-shell2')
 export class AppShell2 extends LitElement {
@@ -19,22 +20,25 @@ export class AppShell2 extends LitElement {
     return [
       css`
         .header-container {
-          padding: 16px;
+          padding: 16px 16px 0px;
           text-align: right;
         }
         .logo {
           text-align: center;
-          padding: 40px 0;
+          padding-top: 8px;
+          padding-bottom: 16px;
         }
+
         .content-container {
           width: 100%;
-          max-width: 450px;
+          max-width: 500px;
+          padding: 0 8px;
         }
         .apps-container {
           display: flex;
           flex-direction: row;
           flex-wrap: wrap;
-          justify-content: space-around;
+          font-size: 12px;
         }
 
         .layout-h {
@@ -42,14 +46,47 @@ export class AppShell2 extends LitElement {
           flex-direction: row;
           justify-content: center;
         }
-        .icon-wrapper {
+        .app-wrapper {
           display: flex;
           flex-direction: column;
           align-items: center;
+          padding: 12px 6px;
+        }
+        .svg-wrapper {
+          height: 55px;
+          width: 55px;
+          display: flex;
+          flex-direction: column;
+          background-color: lightgray;
+          border-radius: 50%;
+          align-items: center;
+          justify-content: center;
         }
         .category {
-          padding-top: 26px;
-          padding-bottom: 16px;
+          color: var(--secondary-text-color);
+          font-weight: bold;
+          font-size: 16px;
+        }
+        .app-name {
+          margin-top: 12px;
+          width: 95px;
+          text-align: center;
+        }
+        .footer {
+          height: 100px;
+        }
+        fieldset {
+          margin-bottom: 20px;
+          border-width: 1px;
+          border-style: solid;
+          border-radius: 8px;
+        }
+        img#profile {
+          background-color: rgba(211, 211, 211, 0.4);
+          border-radius: 50%;
+          padding: 6px;
+          width: 20px;
+          height: 20px;
         }
       `
     ];
@@ -59,55 +96,70 @@ export class AppShell2 extends LitElement {
     // main template
     // language=HTML
     return html`
+      ${appTheme}
       <div class="header-container">
-        <img src="./images/perm_identity-24px.svg" alt="etools Logo" />
+        <img id="profile" src="./images/perm_identity-24px.svg" alt="etools Logo" />
       </div>
       <div class="logo">
         <img id="app-logo" class="logo" src="./images/etools-logo-color-white.svg" alt="eTools" />
       </div>
       <div class="layout-h">
         <div class="content-container">
-          <div class="category">Programme Management</div>
+        <fieldset>
+          <legend class="category">Programme Management</legend>
           <div class="apps-container">
-            <div class="icon-wrapper">
-              ${unppIcon}
-              UN Partner Portal
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${unppIcon}</div>
+              <div class="app-name">UN Partner Portal</div>
             </div>
-            <div class="icon-wrapper">
-              ${pmpIcon}
-              Partnership Management
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${pmpIcon}</div>
+              <div class="app-name">Partnership Management</div>
             </div>
           </div>
-          <div class="category">Monitoring & Assurance</div>
+        </fieldset>
+
+        <fieldset>
+          <legend class="category">Monitoring & Assurance</legend>
           <div class="apps-container">
-            <div class="icon-wrapper">
-              ${tripsIcon}
-              Trip Management
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${tripsIcon}</div>
+              <div class="app-name">Trip Management</div>
             </div>
-            <div class="icon-wrapper">
-              ${tpmIcon}
-              Third Party Monitoring
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${tpmIcon}</div>
+              <div class="app-name">Third Party Monitoring</div>
             </div>
-            <div class="icon-wrapper">
-              ${famIcon}
-              Financial Assurance
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${famIcon}</div>
+              <div class="app-name">Financial Assurance</div>
             </div>
-            <div class="icon-wrapper">
-              ${pseaIcon}
-              PSEA Assurance
+            <div class="app-wrapper">
+            <div class="svg-wrapper">${pseaIcon}</div>
+              <div class="app-name">PSEA Assurance</div>
             </div>
-            <div class="icon-wrapper">
-              ${fmIcon}
-              Field Monitoring
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${fmIcon}</div>
+              <div class="app-name">Field Monitoring</div>
             </div>
           </div>
-          <div class="category">Dashborads & Analytics</div>
+        </fieldset>
+
+          
+        <fieldset>
+          <legend class="category">Dashborads & Analytics</legend>
           <div class="apps-container">
-          <div class="icon-wrapper">${apdIcon} Action Points</div>
-          <div class="icon-wrapper">${dashIcon} Dashboards</div>
-          <div class="icon-wrapper">${powerBiIcon} <div>Implementation Intelligence (I<sup>2</sup>)</div></div>
-          <div class="icon-wrapper">${externalIcon} Datamart</div>
-        </div>
+            <div class="app-wrapper">
+              <div class="svg-wrapper">${apdIcon}</div>
+              <div class="app-name">Action Points</div>
+             </div>
+            <div class="app-wrapper"><div class="svg-wrapper">${dashIcon}</div> <div class="app-name">Dashboards</div></div>
+            <div class="app-wrapper"><div class="svg-wrapper">${powerBiIcon}</div> <div class="app-name">Implementation Intelligence (I<sup>2</sup>)</div></div>
+            <div class="app-wrapper"><div class="svg-wrapper">${externalIcon}</div> <div class="app-name">Datamart</div></div>
+          </div>
+        </fieldset>
+
+        <div class="footer"></div>
       </div>
     `;
   }
