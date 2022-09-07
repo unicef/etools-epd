96597c064a2aa2ad7b977da440ce14c7a3646944
Dan <49279861+danNordlogic@users.noreply.github.com>
2022-09-07 12:12:01 +0300

    [ch31321] Improve the language dropdown (#393)

--- src_ts/components/app-shell/header/page-header.ts
+++ src_ts/components/app-shell/header/page-header.ts
@@ -2,7 +2,9 @@ import '@polymer/app-layout/app-toolbar/app-toolbar';
 import '@polymer/paper-icon-button/paper-icon-button';
 import '@unicef-polymer/etools-profile-dropdown/etools-profile-dropdown';
 import '@unicef-polymer/etools-dropdown/etools-dropdown.js';
-import {customElement, LitElement, html, property} from 'lit-element';
+import {EtoolsDropdownEl} from '@unicef-polymer/etools-dropdown/etools-dropdown';
+
+import {customElement, LitElement, html, property, query} from 'lit-element';
 
 import './countries-dropdown';
 
@@ -110,6 +112,7 @@ export class PageHeader extends connect(store)(LitElement) {
         <div class="header__item header__right-group">
           <div class="dropdowns">
             <etools-dropdown
+              id="languageSelector"
               .selected="${this.selectedLanguage}"
               .options="${this.languages}"
               option-label="display_name"
@@ -191,10 +194,17 @@ export class PageHeader extends connect(store)(LitElement) {
 
   @property() selectedLanguage!: string;
 
+  @query('#languageSelector') private languageDropdown!: EtoolsDropdownEl;
+
   public connectedCallback() {
     super.connectedCallback();
     this.setBgColor();
     this.checkEnvironment();
+
+    setTimeout(() => {
+      const fitInto = document.querySelector('app-shell')!.shadowRoot!.querySelector('#appHeadLayout');
+      this.languageDropdown.set('fitInto', fitInto);
+    }, 0);
   }
 
   public stateChanged(state: RootState) {
