c905f016da315ac17780409b356260235a879e1f
Trif Adriana <trif.corina.adriana@gmail.com>
2022-12-13 18:44:47 +0200

    linting

--- src_ts/config/app-constants.ts
+++ src_ts/config/app-constants.ts
@@ -16,7 +16,7 @@ export const appLanguages: GenericObject<string>[] = [
   {value: 'ar', display_name: 'Arabic'},
   {value: 'pt', display_name: 'Portuguese'},
   {value: 'fr', display_name: 'French'},
-  {value: 'es', display_name: 'Spanish'}
+  {value: 'es', display_name: 'Spanish'},
   {value: 'ru', display_name: 'Russian'}
 ];
 
