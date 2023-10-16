2169938eef666ba75928a82dff2edfb3ae8dc6c9
dcioara <dan.cioara@nordlogic.com>
2023-10-16 11:46:08 +0300

    adjustments

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,6 @@
  */
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
-import filesize from 'rollup-plugin-filesize';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import copy from 'rollup-plugin-copy';
 
