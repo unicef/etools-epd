725354027666088117e1ea75e59c97cb63248ce0
Dan <dan.cioara@nordlogic.com>
2024-12-05 10:37:09 +0200

    [ch38789] Add `etools-translate` component in `eTools-unicef` package and update apps

--- src_ts/app-shell.ts
+++ src_ts/app-shell.ts
@@ -53,7 +53,7 @@ import isEmpty from 'lodash-es/isEmpty';
 import get from 'lodash-es/get';
 import './components/env-flags/environment-flags';
 import '@unicef-polymer/etools-unicef/src/etools-toasts/etools-toasts';
-import {registerTranslateConfig, use, translate} from 'lit-translate';
+import {registerTranslateConfig, use, translate} from '@unicef-polymer/etools-unicef/src/etools-translate';
 import {EtoolsUser, RouteDetails} from '@unicef-polymer/etools-types';
 import {setStore} from '@unicef-polymer/etools-utils/dist/store.util';
 import {SMALL_MENU_ACTIVE_LOCALSTORAGE_KEY} from './config/config';
